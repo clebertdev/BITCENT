7b794dfdfd9192d7870d8889059993d861de595c
ClebertDev <clebertbsb@yahoo.com.br>
2023-05-22 17:26:49 -0300

    redes sociais

--- src/components/landing/rodape/RedesSociais.tsx
+++ src/components/landing/rodape/RedesSociais.tsx
@@ -5,9 +5,9 @@ import RedeSocial from "./RedeSocial";
 export default function RedesSociais() {
     return (
         <div className="flex">
-            <RedeSocial icone={<IconBrandYoutube />} url="https://www.youtube.com/@cod3r"/>
-            <RedeSocial icone={<IconBrandInstagram />} url=""/>
-            <RedeSocial icone={<IconBrandFacebook />} url=""/>
+            <RedeSocial icone={<IconBrandYoutube />} url="https://www.youtube.com"/>
+            <RedeSocial icone={<IconBrandInstagram />} url="https://www.instagram.com/clebertbsb/"/>
+            <RedeSocial icone={<IconBrandFacebook />} url="https://www.facebook.com/clebert.felgueirasmoreira"/>
             <RedeSocial icone={<IconBrandGithub />} url=""/>
 
         </div>
